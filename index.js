8b78359266c2c92d754c863277a545f56b4b1bec
Lisa <misslisazhou@gmail.com>
2016-11-11 21:26:41 -1000

    add path as a dependency

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var path = require('path');
 
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, 'views'));
