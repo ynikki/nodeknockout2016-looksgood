44186aa3dc45ab0d05aaae24d4b032952da82426
sgalutira <sheena.galutira@gmail.com>
2016-11-11 23:04:11 -1000

    randomized plants and zombies

--- public/hangman.js
+++ public/hangman.js
@@ -1,4 +1,5 @@
-function hangman(){
+(function hangman(){
+  var words;
   var wordBank = {
     'plants': ['peashooter', 'sunflower', 'cherry bomb', 'potato mine', 'chomper'],
     'zombies': ['conehead', 'pole vaulting', 'buckethead', 'newspaper', 'dancing']
@@ -13,4 +14,7 @@ function hangman(){
 
   var validLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-}
+  var newPlants = wordBank.plants[Math.floor(Math.random() * wordBank.plants.length)];
+  var newZombies = wordBank.zombies[Math.floor(Math.random() * wordBank.zombies.length)];
+
+}());
