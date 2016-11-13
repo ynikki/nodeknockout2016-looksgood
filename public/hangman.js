6a72fe4835482befc57ea3347ecd6480cc6fb9ee
Christie R <creindle@hawaii.edu>
2016-11-12 15:41:15 -1000

    Fixed javascript

--- public/hangman.js
+++ public/hangman.js
@@ -1,144 +1,3 @@
-// (function () {
-//     var validLetters, words, guessInput, guess, guessButton, guessedLetter, matchedLetter, output, health, letters, currentWord, numMatchedLetter, hp, outcomes;
-
-//     var wordBank = {
-//       'plants': ['peashooter', 'sunflower', 'cherry bomb', 'potato mine', 'chomper'],
-//       'zombies': ['conehead', 'pole vaulting', 'buckethead', 'newspaper', 'dancing']
-//     };
-
-//     var newPlants = wordBank.plants[Math.floor(Math.random() * wordBank.plants.length)];
-//     var newZombies = wordBank.zombies[Math.floor(Math.random() * wordBank.zombies.length)];
-
-//     function setup() {
-//         /* start config options */
-//         validLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-//         hp = 5;
-//         words = ['conehead', 'pole vaulting', 'buckethead', 'newspaper', 'dancing'];
-//         outcomes = {
-//           win: "I'm ALIVE!",
-//           lose: "I withered away :'(",
-//           guessed: 'Try another letter, already guessed',
-//           validation: 'Enter a letter A-Z'
-//         };
-
-//         /* end config options */
-
-//         guessedLetter = matchedLetter = '';
-//         numMatchedLetter = 0;
-
-//         /* choose a word */
-//         currentWord = words[Math.floor(Math.random() * words.length)];
-
-//         /* make #health and #output blank, create vars for later access */
-//         output = document.getElementById("output");
-//         health = document.getElementById("health");
-//         guessInput = document.getElementById("letter");
-
-//         health.innerHTML = 'You have ' + hp + ' hp remaining';
-//         output.innerHTML = '';
-
-//         document.getElementById("letter").value = '';
-
-//         /* make sure guess button is enabled */
-//         guessButton = document.getElementById("guess");
-//         guessInput.style.display = 'inline';
-//         guessButton.style.display = 'inline';
-
-//         /* set up display of letters in current word */
-//         letters = document.getElementById("letters");
-//         letters.innerHTML = '<li class="current-word">Current word:</li>';
-
-//         var letter, i;
-//         for (i = 0; i < currentWord.length; i++) {
-//             letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
-//             letters.insertAdjacentHTML('beforeend', letter);
-//         }
-//     }
-
-//     function endGame(win) {
-//         if (win) {
-//             output.innerHTML = outcomes.win;
-//             output.classList.add('win');
-//         } else {
-//             output.innerHTML = outcomes.lose;
-//             output.classList.add('error');
-//         }
-
-//         guessInput.style.display = guessButton.style.display = 'none';
-//         guessInput.value = '';
-//     }
-
-//     /* Start game - should ideally check for existing functions attached to window.onload */
-//     window.onload = setup();
-
-//      buttons
-//     document.getElementById("restart").onclick = setup;
-
-//     /* reset letter to guess on click */
-//     guessInput.onclick = function () {
-//         this.value = '';
-//     };
-
-//     /* main guess function when user clicks #guess */
-//     document.getElementById('hangman').onsubmit = function (e) {
-//         if (e.preventDefault) e.preventDefault();
-//         output.innerHTML = '';
-//         output.classList.remove('error', 'warning');
-//         guess = guessInput.value;
-
-//         /* does guess have a value? if yes continue, if no, error */
-//         if (guess) {
-//             /* is guess a valid letter? if so carry on, else error */
-//             if (validLetters.indexOf(guess) > -1) {
-//                 /* has it been guessed (missed or matched) already? if so, abandon & add notice */
-//                 if ((matchedLetter && matchedLetter.indexOf(guess) > -1) || (guessedLetter && guessedLetter.indexOf(guess) > -1)) {
-//                     output.innerHTML = '"' + guess.toUpperCase() + '"' + outcomes.guessed;
-//                     output.classList.add("warning");
-//                 }
-//                 /* does guess exist in current word? if so, add to letters already matched, if final letter added, game over with win message */
-//                 else if (currentWord.indexOf(guess) > -1) {
-//                     var lettersToShow;
-//                     lettersToShow = document.querySelectorAll(".letter" + guess.toUpperCase());
-
-//                     for (var i = 0; i < lettersToShow.length; i++) {
-//                         lettersToShow[i].classList.add("correct");
-//                     }
-
-//                     /* check to see if letter appears multiple times */
-//                     for (var j = 0; j < currentWord.length; j++) {
-//                         if (currentWord.charAt(j) === guess) {
-//                             numMatchedLetter += 1;
-//                         }
-//                     }
-
-//                     matchedLetter += guess;
-//                     if (numMatchedLetter === currentWord.length) {
-//                         endGame(true);
-//                     }
-//                 }
-//                 /* guess doesn't exist in current word and hasn't been guessed before, add to guessedLetter, reduce hp & update user */
-//                 else {
-//                     guessedLetter += guess;
-//                     hp--;
-//                     health.innerHTML = 'You have ' + hp + ' hp remaining';
-//                     if (hp === 0) endGame();
-//                 }
-//             }
-//             /* not a valid letter, error */
-//             else {
-//                 output.classList.add('error');
-//                 output.innerHTML = outcomes.validation;
-//             }
-//         }
-//         /* no letter entered, error */
-//         else {
-//             output.classList.add('error');
-//             output.innerHTML = outcomes.validation;
-//         }
-//         return false;
-//     };
-// }());
-
 window.onload = function () {
 
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -226,77 +85,6 @@ window.onload = function () {
     }
   }
 
-      // Animate man
-  // var animate = function () {
-  //   var drawMe = lives ;
-  //   drawArray[drawMe]();
-  // }
-
-
-   // Hangman
-  // canvas =  function(){
-
-  //   myStickman = document.getElementById("stickman");
-  //   context = myStickman.getContext('2d');
-  //   context.beginPath();
-  //   context.strokeStyle = "#fff";
-  //   context.lineWidth = 2;
-  // };
-
-    // head = function(){
-    //   myStickman = document.getElementById("stickman");
-    //   context = myStickman.getContext('2d');
-    //   context.beginPath();
-    //   context.arc(60, 25, 10, 0, Math.PI*2, true);
-    //   context.stroke();
-    // }
-
-//   draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
-
-//     context.moveTo($pathFromx, $pathFromy);
-//     context.lineTo($pathTox, $pathToy);
-//     context.stroke();
-// }
-
-//    frame1 = function() {
-//      draw (0, 150, 150, 150);
-//    };
-
-//    frame2 = function() {
-//      draw (10, 0, 10, 600);
-//    };
-
-//    frame3 = function() {
-//      draw (0, 5, 70, 5);
-//    };
-
-//    frame4 = function() {
-//      draw (60, 5, 60, 15);
-//    };
-
-//    torso = function() {
-//      draw (60, 36, 60, 70);
-//    };
-
-//    rightArm = function() {
-//      draw (60, 46, 100, 50);
-//    };
-
-//    leftArm = function() {
-//      draw (60, 46, 20, 50);
-//    };
-
-//    rightLeg = function() {
-//      draw (60, 70, 100, 100);
-//    };
-
-//    leftLeg = function() {
-//      draw (60, 70, 20, 100);
-//    };
-
-  // drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1];
-
-
   // OnClick Function
    check = function () {
     list.onclick = function () {
@@ -313,7 +101,6 @@ window.onload = function () {
       if (j === -1) {
         lives -= 1;
         comments();
-        // animate();
       } else {
         comments();
       }
@@ -342,7 +129,6 @@ window.onload = function () {
     result();
     comments();
     selectCat();
-    //canvas();
   }
 
   play();
@@ -368,10 +154,10 @@ window.onload = function () {
     correct.parentNode.removeChild(correct);
     letters.parentNode.removeChild(letters);
     showClue.innerHTML = "";
-    //context.clearRect(0, 0, 400, 400);
     play();
   }
 }
+
 function endGame(win) {
     var container = document.getElementById('end-img');
     var endImageDiv = document.createElement('div');
