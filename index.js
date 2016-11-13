3570b150cc4b98a8a74dd8efc3061941beabb00e
sgalutira <sheena.galutira@gmail.com>
2016-11-13 03:33:41 -1000

    removed cors

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ app.set('views', path.resolve(__dirname, 'views'));
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-// CORS //
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
-});
-
 app.get('/', function(req, res) {
   res.render('index')
 })
