05a8d6f8ece3203246d097c228084cc3ff539471
Nikki K <808nikz@gmail.com>
2016-11-12 19:30:40 -1000

    Animating the eyes

--- public/js/animate.js
+++ public/js/animate.js
@@ -1,1 +1,78 @@
-var blinkingFace = $.keyframe.isSupported();
+var supportedFlag = $.keyframe.isSupported();
+$.keyframe.debug = true;
+
+$.keyframe.define([
+  {
+    name: 'leftEyeBlink',
+      '0%': {
+        'height': '20px',
+      },
+      '50%': {
+        'height': '2px',
+        'width': '15px',
+      },
+      '100%': {
+        'height': '20px',
+      }
+  },
+  {
+    name: 'rightEyeBlink',
+    '0%': {
+      'height': '20px',
+    },
+    '50%': {
+      'height': '2px',
+      'width': '15px',
+    },
+    '100%': {
+      'height': '35px',
+    }
+  }
+]);
+
+var interval = 3000;
+
+function blink() {
+  $("#leftEye").playKeyframe({
+    name: 'leftEyeBlink',
+    duration: '200ms',
+    timingFunction: 'linear',
+    complete: function() {
+      $("#leftEye").resetKeyframe();
+    }
+  });
+  $("#rightEye").playKeyframe({
+    name: 'rightEyeBlink',
+    duration: '200ms',
+    timingFunction: 'linear',
+    complete: function() {
+      $("#rightEye").resetKeyframe();
+    }
+  });
+  interval = Math.random()*(6000-3000)+3000;
+  setTimeout(blink, interval);
+}
+
+setTimeout(blink, interval);
+
+$("#leftEye").click(function() {
+  $("#leftEye").playKeyframe({
+    name: 'leftEyeBlink',
+    duration: '200ms',
+    timingFunction: 'linear',
+    complete: function() {
+      $("#leftEye").resetKeyframe();
+    }
+  });
+});
+
+$("#rightEye").click(function() {
+  $("#rightEye").playKeyframe({
+    name: 'rightEyeBlink',
+    duration: '200ms',
+    timingFunction: 'linear',
+    complete: function() {
+      $("#rightEye").resetKeyframe();
+    }
+  });
+});
